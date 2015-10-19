e0a6847a356d5cc7bb903d05f4697f4c8ba79a24
Dan Crisan <dancrisan@Dans-MacBook-Air.local>
2015-10-18 20:09:53 -0400

    Initial Commit

---
+++ around_alpha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  around_alpha
+//
+//  Created by Dan Crisan on 2015-10-18.
+//  Copyright (c) 2015 Dan Crisan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
